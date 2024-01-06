e1de1ab5effa69abca9f1d7af68bea349dccb4a1
aoigj100a <aoigj100@gmail.com>
2024-01-06 10:49:18 +0800

    style: 調整簡介與微調間距、分段

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,17 +9,18 @@ export default function Home() {
         <div className="flex ">
           <div className="pt-[400px] pb-[100px] z-10 px-[8px] lg:px-[320px]">
 
-            <h1 className="pl-[80px] lg:pl-[16px] text-left lg:block flex-1 font-bold underline text-pink-300 pb-[16px]">
+            <h1 className="pl-[40px] lg:pl-[16px] text-left lg:block flex-1 font-bold underline text-pink-300 pb-[24px]">
               <p>Jenny Liou.</p>
               <p>Pleased to meet you.</p>
             </h1>
-            <div className="pl-[94px] lg:pl-[32px] text-pink-300">
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting</p>
-              <p>industry. Lorem Ipsum has been the industry &apos; s standard dummy text</p>
-              <p>ever since the 1500s, when an unknown printer took a galley of</p>
-              <p>type and scrambled it to make a type specimen book. It has</p>
-              <p>survived not only five centuries, but also the leap into</p>
-              <p>electronic typesetting, remaining essentially unchanged.</p>
+            <div className="lg:pl-[32px] text-pink-300">
+              <p>畢業於國立雲林科技大學數位媒體設計系，</p>
+              <p>是一位設計背景的前端工程師</p>
+              <p>擁有 2 年前端工作經驗，專長為網頁前端開發跨平台網站、串接功能，致力創造最好的使用者體驗。</p>
+              <br />
+              <p>尋找前端開發相關之工作職位，願意學習後端技能，爭取全端工程師職位。</p>
+              <p>尋找前端開發相關之工作職位，</p>
+              <p>願意學習後端技能，爭取全端工程師職位。</p>
             </div>
           </div>
           <div className="hidden lg:flex flex-1 text-white justify-end">
